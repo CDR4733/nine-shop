fb3cd912b42b7937186364f280b75cdbb54261da
CDR4733 <shsing2@naver.com>
2024-05-17 11:57:11 +0900

    Update2 index.js - .env issue

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -2,15 +2,16 @@ import mongoose from "mongoose";
 // .env 파일 사용을 위해서는 여기에 패키지 import가 필요합니다.
 import dotenv from "dotenv";
 dotenv.config();
+console.log(process.env.MONGODB_NAME, process.env.MONGODB_URL);
 
 const connect = () => {
   // mongoose.connect는 MongoDB 서버에 연결하는 메서드입니다.
   mongoose
     .connect(
       // .env 파일에 민감한 정보를 추가해 사용합니다.
-      "mongodb+srv://sparta-user:aaaa4321@express-mongo.bgtlgzi.mongodb.net/?retryWrites=true&w=majority&appName=express-mongo",
+      process.env.MONGODB_URL,
       {
-        dbName: "nine_shop",
+        dbName: process.env.MONGODB_NAME,
       },
     )
     .then(() => console.log("MongoDB 연결에 성공하였습니다."))
