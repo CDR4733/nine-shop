a05e9cf5ca8da86a1f291438df9d00869c7d5876
CDR4733 <shsing2@naver.com>
2024-05-17 11:06:50 +0900

    Update2 app.js

--- src/app.js
+++ src/app.js
@@ -17,9 +17,17 @@ app.use(express.urlencoded({ extended: true }));
 
 const router = express.Router();
 
-// router.get("/", (req, res) => {
-//   return res.json({ message: "NINE-SHOP!" });
-// });
+router.get("/", (req, res) => {
+  return res.json({
+    message1: "NINE-SHOP!",
+    message2: "1. 상품 생성 API [POST] '/products'",
+    message3: "2. 상품 조회 API [GET] '/products'",
+    message4: "3. 상품 상세 조회 API [GET] '/products/:id'",
+    message5: "4. 상품 수정 API [PATCH] '/products/:id'",
+    message6: "5. 상품 삭제 API [DELETE] '/products/:id'",
+    message7: "THANK-YOU!",
+  });
+});
 
 // router와 productsRouter로 클라이언트의 요청이 전달된다!
 app.use("/", [router, productsRouter]);
